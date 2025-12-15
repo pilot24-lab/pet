096f3f18d2a83d2188e320e0b27e1c290b0c15ef
Nikita <nickita.rfonline@yandex.ru>
2025-12-15 07:11:53 +0300

    update migrations

--- src/infrastructure/database/migrations/004_create_comments_table.sql
+++ src/infrastructure/database/migrations/004_create_comments_table.sql
@@ -1,14 +1,9 @@
 create table if not exists comments (
     id serial primary key,
-    user_id integer not null,
+    user_id integer references users(id) not null,
     comment text not null,
     created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp,
-
-    constraint fk_comments_user
-        foreign key (user_id)
-        references users(id)
-        on delete cascade
+    updated_at timestamp default current_timestamp   
 );
 
 create index if not exists idx_users_id on comments(user_id);
